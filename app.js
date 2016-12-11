70b43fa14b59d9f39ceff4f5ea35eb384bab469d
yupeng07 <yupeng07@baidu.com>
2016-12-11 16:50:31 +0800

    static

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var koa = require('koa');
+var server = require('koa-static');
 var app = koa();
 
-app.use(function *(){
-  this.body = 'Hello World';
-});
+var opts = {
+  maxage: 1000 * 60 * 60 * 24 * 365, // 1年，默认为0
+  hidden: false, // 能否返回隐藏文件（以`.`打头），默认false不返回
+  index: 'index.html', // 默认文件名
+  defer: true, // 在yield* next之后返回静态文件，默认在之前
+  gzip: true 
+  // 允许传输gzip，如静态文件夹下有两个文件，index.js和index.js.gz，
+  // 会优先传输index.js.gz，默认开启
+};
+app.use(server('~/www/static', opts));
 
-app.listen(80);
+// app.use(function *(){
+//   this.body = 'Hello World';
+// });
+
+app.listen(8003);
